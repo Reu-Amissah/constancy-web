48a0abf2393d24e63860e881fae3b38ddbda9a76
Reu-Amissah <reujoyamissah@gmail.com>
2024-01-09 10:49:18 +0000

    navbar mobile

--- js/about.js
+++ js/about.js
@@ -53,3 +53,27 @@ document.querySelectorAll(".phil-items").forEach((item, i) => {
     "-=0.4"
   );
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  let menuButton = document.getElementById("mobile-menu");
+  let menuItems = document.getElementById("drop-items");
+
+  let tabletBreakpoint = 768;
+
+  function adjustMenuDisplay() {
+    if (window.innerWidth <= tabletBreakpoint) {
+      menuItems.style.display = "none";
+    }
+  }
+
+  window.addEventListener("resize", adjustMenuDisplay);
+
+  menuButton.addEventListener("click", function () {
+    menuItems.style.display =
+      menuItems.style.display === "block" ? "none" : "block";
+  });
+
+  if (menuButton.style.display === "none") {
+    menuItems.style.display = "none";
+  }
+});
--- js/index.js
+++ js/index.js
@@ -19,3 +19,28 @@ sections.forEach((section) => {
     duration: 1,
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  let menuButton = document.getElementById("mobile-menu");
+  let menuItems = document.getElementById("drop-items");
+
+  let tabletBreakpoint = 768;
+
+
+  function adjustMenuDisplay() {
+    if (window.innerWidth <= tabletBreakpoint) {
+      menuItems.style.display = "none";
+    }
+  }
+
+  window.addEventListener("resize", adjustMenuDisplay);
+
+  menuButton.addEventListener("click", function () {
+    menuItems.style.display =
+      menuItems.style.display === "block" ? "none" : "block";
+  });
+
+  if (menuButton.style.display === "none") {
+    menuItems.style.display = "none";
+  }
+});
